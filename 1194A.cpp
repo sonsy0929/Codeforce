8c124769a83471bd7c52d3d4b6cab897b19cead9
heize <sonsy0929@naver.com>
2019-07-22 01:24:59 +0900

    Remove a Progression

---
+++ 1194A.cpp
@@ -0,0 +1,15 @@
+/*
+영어 해석 어려워..
+*/
+
+#include <cstdio>
+using namespace std;
+int main(){
+    int T;
+    scanf("%d", &T);
+    while(T--){
+        int x, n;
+        scanf("%d %d", &x, &n);
+        printf("%d\n", n * 2);
+    }
+}
